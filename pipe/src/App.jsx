854a990aa6293dfb66ca49f58a2e9c62b5f7bb5f
S KUMAR <hk856856@gmail.com>
2025-03-17 17:19:59 +0530

    added brands

--- pipe/src/App.jsx
+++ pipe/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PipeoFooter from './components/PipeoFooter'
 import FAQSection from './components/FAQSection'
 import BlogCarousel from './components/BlogCarousel'
+import TrustedBrands from './components/TrustedBrands'
 
 const App = () => {
   return (
     <>
     <FAQSection/>
     <BlogCarousel/>
+    <TrustedBrands/>
     <PipeoFooter/>
       
     </>
