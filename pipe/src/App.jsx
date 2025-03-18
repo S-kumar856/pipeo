93f038b227123275e22129e0ce4f8fb59a258a1a
S KUMAR <hk856856@gmail.com>
2025-03-18 13:25:57 +0530

    created banner

--- pipe/src/App.jsx
+++ pipe/src/App.jsx
@@ -3,13 +3,17 @@ import PipeoFooter from './components/PipeoFooter'
 import FAQSection from './components/FAQSection'
 import BlogCarousel from './components/BlogCarousel'
 import TrustedBrands from './components/TrustedBrands'
+import BannerSection from './components/BannerSection'
+
 
 const App = () => {
   return (
     <>
+    <BannerSection/>
     <FAQSection/>
     <BlogCarousel/>
     <TrustedBrands/>
+
     <PipeoFooter/>
       
     </>
