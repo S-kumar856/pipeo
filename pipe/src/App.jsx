c37f9d947a965aac66ba9651368f9671c45d65e2
S KUMAR <hk856856@gmail.com>
2025-03-21 22:35:22 +0530

    modify service page

--- pipe/src/App.jsx
+++ pipe/src/App.jsx
@@ -5,6 +5,7 @@ import BlogCarousel from './components/BlogCarousel'
 import TrustedBrands from './components/TrustedBrands'
 import BannerSection from './components/BannerSection'
 import PlumbingService from './components/PlumbingService'
+import Error404 from './components/Error404'
 
 
 const App = () => {
@@ -15,7 +16,7 @@ const App = () => {
     <BlogCarousel/>
     <TrustedBrands/>
     <PlumbingService/>
-
+    <Error404/>
     <PipeoFooter/>
       
     </>
